7a5f7bbac98a00ac204b7143d94a8624355acefb
Grzegorz Laszczak <grzegorz.laszczak@gmail.com>
2020-11-11 17:15:30 +0100

    Use async provider example

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,5 +1,6 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
@@ -7,24 +8,26 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    /* do something ... */
-    return ['firstBrand', 'secondBrand'];
-  }
-}
-
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      // Note "async" here, and Promise/Async event inside the Factory function
+      // Could be a database connection / API call / etc
+      // In our case we're just "mocking" this type of event with a Promise
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('Select * FROM ...'); // Use with DB
+        const coffeeBrands = await Promise.resolve([
+          'firstBrand',
+          'secondBrand',
+        ]); // our example mock
+        console.log('[!] Async factory'); // To show thad data form DB will be shown first (before constructor in coffes-service.ts)
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ],
   exports: [CoffeesService],
