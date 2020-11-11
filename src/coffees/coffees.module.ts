f1629b2c1d5909912b36fd38b7628dafa985b41e
Grzegorz Laszczak <grzegorz.laszczak@gmail.com>
2020-11-11 16:40:34 +0100

    Use mock version of class for testing

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,18 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+// to override a class with a mock version for testing
+class MockCoffesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
-  providers: [{ provide: CoffeesService, useClass: CoffeesService }], // in shorter version: providers: [CoffeesService]
-  exports: [CoffeesService]
+  providers: [
+    {
+      provide: CoffeesService,
+      useValue: new MockCoffesService(), // <-- mock implementation
+    },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
