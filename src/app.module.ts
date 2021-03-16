83d508e9f6b3ab6de17f3ace96e8b7edacbcda30
Grzegorz <grzegorz.laszczak@gmail.com>
2021-03-16 09:59:27 +0100

    Implement ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,18 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule, TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     autoLoadEntities: true,
     synchronize: true // (recommended: disable in the production)!!!
   }), CoffeeRatingModule, DatabaseModule],
