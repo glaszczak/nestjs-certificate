f6cd6bf5406b3b647edb082bcedffc06b18f00b2
Grzegorz <grzegorz.laszczak@gmail.com>
2021-03-16 10:06:39 +0100

    Split .env into two files

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,15 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(
+      { envFilePath: ['.environment', '.env.db'] },
+    ),
     CoffeesModule, TypeOrmModule.forRoot({
     type: 'postgres',
     host: process.env.DATABASE_HOST,
     port: +process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE_NAME,
     autoLoadEntities: true,
     synchronize: true // (recommended: disable in the production)!!!
