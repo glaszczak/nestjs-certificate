9ccc44a12c2ca825907e85beeb12cbfc0275fe67
Grzegorz <grzegorz.laszczak@gmail.com>
2021-03-16 17:20:47 +0100

    Create custom decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
 import { APP_PIPE } from '@nestjs/core';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { APP_PIPE } from '@nestjs/core';
     }),
     CoffeeRatingModule,
     DatabaseModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,18 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
-
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
 
+    const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Authorization');
 
     // check it with request to any endpoint using Header: Authorization, value: process.env.API_KEY
-    return authHeader === process.env.API_KEY;
+    return authHeader === this.configService.get('API_KEY') 
   }
 }
